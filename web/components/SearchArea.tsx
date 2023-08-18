320b2a5f26e8b76381a3c4d43d5e6bf89be6de38
naka-12 <104970808+naka-12@users.noreply.github.com>
2023-08-18 10:44:31 +0900

    検索エリアを追加

--- web/components/SearchArea.tsx
+++ web/components/SearchArea.tsx
@@ -1,22 +1,13 @@
-import { Button } from "@mui/material";
-import AddIcon from "@mui/icons-material/Add";
+import SearchBar from "./SearchBar";
+import PaperList from "./PaperList";
+import BottomBar from "./BottomBar";
 
 export default function SearchArea() {
   return (
-    <div
-      style={{
-        display: "flex",
-        backgroundColor: "#ffffff",
-        padding: "6px 30px 6px 6px",
-        gap: "6px",
-        height: "36px",
-        alignItems: "center",
-      }}
-    >
-      <Button variant="outlined" color="inherit">
-        <AddIcon />
-        New
-      </Button>
-    </div>
+    <>
+      <SearchBar />
+      <PaperList />
+      <BottomBar />
+    </>
   );
 }
---
+++ web/components/SearchBar.tsx
@@ -0,0 +1,45 @@
+import { Button, Input, InputAdornment } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import SearchIcon from "@mui/icons-material/Search";
+
+export default function SearchBar() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        backgroundColor: "#ffffff",
+        padding: "6px 30px 6px 6px",
+        gap: "6px",
+        height: "36px",
+        alignItems: "center",
+        borderBottom: "1px solid #e0e0e0",
+      }}
+    >
+      <Button variant="outlined" color="inherit">
+        <AddIcon />
+        New
+      </Button>
+      <Input
+        id="input-with-icon-textfield"
+        placeholder="Search"
+        startAdornment={
+          <InputAdornment position="start">
+            <SearchIcon />
+          </InputAdornment>
+        }
+        disableUnderline
+        style={{
+          display: "flex",
+          width: "400px",
+          height: "36px",
+          padding: "0px 8px",
+          alignItems: "center",
+          gap: "8px",
+          flexShrink: "0",
+          borderRadius: "4px",
+          border: "1px solid #BDBDBD",
+        }}
+      />
+    </div>
+  );
+}
