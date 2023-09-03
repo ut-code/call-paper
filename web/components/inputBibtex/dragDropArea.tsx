5254211a95ebe25f6798f7826be9608038291e8c
subPC <anan-takahiro@g.ecc.u-tokyo.ac.jp>
2023-09-03 22:57:45 +0900

    advice2

--- web/components/inputBibtex/dragDropArea.tsx
+++ web/components/inputBibtex/dragDropArea.tsx
@@ -1,34 +1,29 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback } from "react";
 import { useDropzone } from "react-dropzone";
 import { usePaperInfosContext, useSetPaperInfosContext } from "../contexts";
 import fileToPaperInfo from "./fileToPaperInfo";
-import type { PaperInfo } from "../../src/App";
 
 async function loadFile(file) {
   return new Promise((resolve) => {
     const reader = new FileReader();
     reader.readAsText(file);
     reader.onload = () => {
-      const content = fileToPaperInfo(reader.result);
+      const content = fileToPaperInfo(String(reader.result));
       resolve(content);
     };
   });
 }
 
 function BibtexFileReader({ children }) {
-  console.log("BibtexFileReader");
   const paperInfos = usePaperInfosContext();
   const setPaperInfos = useSetPaperInfosContext();
-  const [newPapers, setNewPapers] = useState<PaperInfo[]>([]);
-  console.log("useState", newPapers.length);
-  const onDrop = useCallback(async (acceptedFiles) => {
-    const theNewPapers = await Promise.all(acceptedFiles.map(loadFile));
-    setNewPapers(theNewPapers);
-  }, []);
-  useEffect(() => {
-    setPaperInfos([...paperInfos, ...newPapers]);
-  }, [newPapers]);
-
+  const onDrop = useCallback(
+    async (acceptedFiles) => {
+      const newPapers = await Promise.all(acceptedFiles.map(loadFile));
+      setPaperInfos([...paperInfos, ...newPapers]);
+    },
+    [paperInfos, setPaperInfos]
+  );
   const { getRootProps, getInputProps } = useDropzone({
     onDrop,
     accept: { "Bibtex/bibtex": [".bibtex"] }, // Specify accepted file types (e.g., .txt)
